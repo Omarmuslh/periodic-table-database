650dfc82ae278738d2da28ad02787dd043c494d7
Omarmuslh <muslhomar32@gmail.com>
2026-09-21 19:35:09 -0400

    fix: adjust element.sh script exact output format

--- element.sh
+++ element.sh
@@ -3,21 +3,32 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
-  echo "Please provide an element as an argument."
+  echo -e "Please provide an element as an argument."
 else
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1;")
+    QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1;"
   else
-    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol ILIKE '$1' OR name ILIKE '$1';")
+    QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol ILIKE '$1' OR name ILIKE '$1';"
   fi
 
-  if [[ -z $ELEMENT ]]
+  RESULT=$($PSQL "$QUERY")
+
+  if [[ -z $RESULT ]]
   then
-    echo "I could not find that element in the database."
+    echo -e "I could not find that element in the database."
   else
-    echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING BOILING
+    echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING BOILING
     do
+      # Clean potential whitespaces from psql output
+      ATOMIC_NUMBER=$(echo $ATOMIC_NUMBER | xargs)
+      NAME=$(echo $NAME | xargs)
+      SYMBOL=$(echo $SYMBOL | xargs)
+      TYPE=$(echo $TYPE | xargs)
+      ATOMIC_MASS=$(echo $ATOMIC_MASS | xargs)
+      MELTING=$(echo $MELTING | xargs)
+      BOILING=$(echo $BOILING | xargs)
+
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
     done
   fi
