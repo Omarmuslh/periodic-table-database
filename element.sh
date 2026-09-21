2112d96d05e3e23e33af727a1077907d3b88e786
Omarmuslh <muslhomar32@gmail.com>
2026-09-21 19:42:11 -0400

    fix: update script output format to match strict test criteria

--- element.sh
+++ element.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only --no-align -c"
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
+  # تحقق إذا كان المدخل رقماً أم نصاً
   if [[ $1 =~ ^[0-9]+$ ]]
   then
     QUERY="SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1;"
@@ -20,15 +22,6 @@ else
   else
     echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING BOILING
     do
-      # إزالة الفراغات الزائدة لتتطابق المخرجات مع اختبار الموقع حرفياً
-      ATOMIC_NUMBER=$(echo $ATOMIC_NUMBER | xargs)
-      NAME=$(echo $NAME | xargs)
-      SYMBOL=$(echo $SYMBOL | xargs)
-      TYPE=$(echo $TYPE | xargs)
-      ATOMIC_MASS=$(echo $ATOMIC_MASS | xargs)
-      MELTING=$(echo $MELTING | xargs)
-      BOILING=$(echo $BOILING | xargs)
-
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
     done
   fi
